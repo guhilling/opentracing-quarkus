a9b078181d195fcc9a5230df7ba3f475c467e076
Gunnar Hilling <gunnar@hilling.de>
2020-09-08 18:01:04 +0200

    add additional trace

--- quarkus-world/src/main/java/it/hilling/training/opentracing/DatabaseResource.java
+++ quarkus-world/src/main/java/it/hilling/training/opentracing/DatabaseResource.java
@@ -1,13 +1,27 @@
 package it.hilling.training.opentracing;
 
+import io.agroal.api.AgroalDataSource;
 import org.eclipse.microprofile.opentracing.Traced;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 
 @RequestScoped
 public class DatabaseResource {
+
+    @Inject
+    AgroalDataSource dataSource;
+
     @Traced(operationName = "db_access")
     public String translate(String arg) {
+        try(Connection connection=dataSource.getConnection()) {
+            connection.createStatement().execute("SELECT * FROM demo_user left join demo_group dg on demo_user.group_id = dg.group_id;");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return arg.toUpperCase();
     }
 }
--- quarkus-world/src/test/java/it/hilling/training/opentracing/WorldResourceImplTest.java
+++ quarkus-world/src/test/java/it/hilling/training/opentracing/WorldResourceImplTest.java
@@ -1,12 +1,18 @@
 package it.hilling.training.opentracing;
 
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
-@QuarkusTest
+@Disabled
+@QuarkusTest()
+@QuarkusTestResource(H2DatabaseTestResource.class)
 public class WorldResourceImplTest {
 
     @Test
