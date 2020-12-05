bf15960e4d0d83a46c2f80ac2f4d2b0d75e54008
Gunnar Hilling <gunnar@hilling.de>
2020-12-05 17:05:27 +0100

    fix imports and explicit cast.

--- quarkus-hello/src/main/java/it/hilling/training/opentracing/DemoResource.java
+++ quarkus-hello/src/main/java/it/hilling/training/opentracing/DemoResource.java
@@ -1,14 +1,15 @@
 package it.hilling.training.opentracing;
 
-import io.opentracing.*;
+import io.opentracing.References;
+import io.opentracing.Scope;
+import io.opentracing.Span;
+import io.opentracing.Tracer;
 import io.opentracing.log.Fields;
 import io.opentracing.tag.Tags;
 
 import javax.inject.Inject;
-import javax.validation.constraints.NotNull;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.Map;
@@ -26,13 +27,13 @@ public class DemoResource {
                 .withTag(Tags.COMPONENT.getKey(), "demo")
                 .start();
         tracer.scopeManager().activate(span, true);
-        for(int i=0; i<5; i++) {
+        for (int i = 0; i < 5; i++) {
             createChildSpan(i);
         }
-        for(int i=0; i<5; i++) {
+        for (int i = 0; i < 5; i++) {
             createReferencingSpan(i);
         }
-        for(int i=0; i<5; i++) {
+        for (int i = 0; i < 5; i++) {
             createIndependantSpan(i);
         }
         span.finish();
@@ -44,8 +45,8 @@ public class DemoResource {
                 .withTag("mytag", "a child span")
                 .start();
         try (Scope scope = tracer.scopeManager().activate(span, true)) {
-            sleep(10 * index);
-        } catch(Exception ex) {
+            sleep(10L * index);
+        } catch (Exception ex) {
             Tags.ERROR.set(span, true);
             span.log(Map.of(Fields.EVENT, "error", Fields.ERROR_OBJECT, ex, Fields.MESSAGE, ex.getMessage()));
         } finally {
@@ -59,8 +60,8 @@ public class DemoResource {
                 .addReference(References.FOLLOWS_FROM, tracer.activeSpan().context())
                 .withTag("mytag", "follows from")
                 .start();
-        try(Scope scope = tracer.scopeManager().activate(span, true)) {
-            sleep(10 * index);
+        try (Scope scope = tracer.scopeManager().activate(span, true)) {
+            sleep(10L * index);
         } finally {
             span.finish();
         }
@@ -71,8 +72,8 @@ public class DemoResource {
                 .ignoreActiveSpan()
                 .withTag("mytag", "independant")
                 .start();
-        try(Scope scope = tracer.scopeManager().activate(span, true)) {
-            sleep(10 * index);
+        try (Scope scope = tracer.scopeManager().activate(span, true)) {
+            sleep(10L * index);
         } finally {
             span.finish();
         }
